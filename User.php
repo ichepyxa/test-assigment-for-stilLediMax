2e6ccd5805a14abb18b8279d234db0af2ac9f80d
Максим Сарамудов <maxim.krytizna@gmail.com>
2022-11-03 00:38:54 +0300

    feat: done user

--- User.php
+++ User.php
@@ -5,6 +5,8 @@
  * 
  * Дата реализации: 02.11.2022 13:20
  * 
+ * Дата изменения: 03.11.2022 00:38
+ * 
  * Утилита для работы с базой данных
  */
 
@@ -12,21 +14,33 @@ declare(strict_types=1);
 require_once "./connection.php";
 
 /**
- * TODO:
- * [+] Создать функцию форматирование человека
- * [*] Исправить конструктор.
+ * Класс User
+ * Класс предназначен для работы с базой данных. Класс осуществляет сохранение, удаление, форматирование пользователя, преобразование пола из двоичной системы в строку и преобразование даты рождения в количество полных лет.
  */
 class User 
 {
-  public int $id;
-  public string $name;
-  public string $surname;
-  public DateTime $dateOfBirth;
-  public int $gender;
-  public string $cityBirth; 
+  protected int $id;
+  protected string $name;
+  protected string $surname;
+  protected DateTime $dateOfBirth;
+  protected int $gender;
+  protected string $cityBirth;
 
+  /**
+   * Конструктор класса User.
+   * Конструктор предназначен для инициализации полей класса. 
+   * Если переданный id существует в базе данных, то поля заполняются полученными данными из базы данных, иначе валидируются переданные параметры и в случае успеха поля заполняются этими данными.
+   * 
+   * @param int $id
+   * @param string $name
+   * @param string $surname
+   * @param DateTime $dateOfBirth
+   * @param int $gender
+   * @param string $cityBirth
+   * @return void
+   */
   function __construct(int $id, string $name = '', string $surname = '',
-    DateTime $dateOfBirth = new DateTime('now'), int $gender = 0, string $cityBirth = ''
+    DateTime $dateOfBirth = null, int $gender = 0, string $cityBirth = ''
   ) {
     $query = $GLOBALS['pdo']->prepare("SELECT * FROM `users` WHERE `id` = :id");
     $query->execute(array(
@@ -41,9 +55,8 @@ class User
       $this->dateOfBirth = new DateTime($result['date_of_birth']);
       $this->cityBirth = $result['city_birth'];
       $this->gender = (int)$result['gender'];
-      echo '<pre>';
-      var_dump($this);
-      echo '</pre>';
+
+      echo 'Пользователь уже есть в базе данных';
       return;
     }
 
@@ -68,12 +81,18 @@ class User
     $this->id = $id;
     $this->name = $name;
     $this->surname = $surname;
-    $this->dateOfBirth = $dateOfBirth;
+    $this->dateOfBirth = $dateOfBirth ? new DateTime('now') : $dateOfBirth;
     $this->cityBirth = $cityBirth;
     $this->gender = $gender;
+    $this->save();
   }
 
-  public function save()
+  /**
+   * Сохранение пользователя в базу данных.
+   * 
+   * @return void
+   */
+  public function save(): void
   {
     try {
       $query = $GLOBALS['pdo']->prepare("INSERT INTO `users` (`id`, `name`, `surname`, `date_of_birth`, `gender`, `city_birth`) VALUES (:id, :name, :surname, :dateOfBirth, :gender, :cityBirth)");
@@ -86,13 +105,18 @@ class User
         'cityBirth' => $this->cityBirth,
       ));
 
-      echo "Успешное сохранения пользователя";
+      echo "Успешное сохранение пользователя";
     } catch (PDOException $e) {
       die("Ошибка сохранения пользователя: {$e->getMessage()}");
     }
   }
 
-  public function delete()
+  /**
+   * Удаление пользователя из базы данных по полю id.
+   * 
+   * @return void
+   */
+  public function delete(): void
   {
     try {
       $query = $GLOBALS['pdo']->prepare("DELETE FROM `users` WHERE `id` = :id");
@@ -100,26 +124,59 @@ class User
         'id' => $this->id,
       ));
 
-      echo "Успешное удаления пользователя с id = {$this->id}";
+      echo "Успешное удаление пользователя с id = {$this->id}";
     } catch (PDOException $e) {
       die("Ошибка удаления пользователя: {$e->getMessage()}");
     }
   }
 
-  static public function formatDateOfBirthInYears(DateTime $dateOfBirth)
+  /**
+   * Форматирование пользователя с преобразованием возраста и даты рождения.
+   * 
+   * @return stdClass
+   */
+  public function formatUser(): stdClass
   {
-    $years = floor((time() - $dateOfBirth->getTimestamp()) / (60 * 60 * 24 * 365));
-    return $years;
+    $years = self::convertDateOfBirthInYears($this->dateOfBirth);
+    $gender = self::convertGenderInString($this->gender);
+
+    $object = new stdClass();
+    $object->id = $this->id;
+    $object->name = $this->name;
+    $object->surname = $this->surname;
+    $object->dateOfBirth = $years;
+    $object->cityBirth = $this->cityBirth;
+    $object->gender = $gender;
+
+    return $object;
   }
 
+  /**
+   * Преобразование пола из двоичной системы в строку.
+   * 
+   * @param int $gender
+   * @return string
+   */
+  static public function convertGenderInString(int $gender): string
+  {
+    if ($gender === 0) {
+      return 'Муж';
+    } else if ($gender === 1) {
+      return 'Жен';
+    }
+  }
 
+  /**
+   * Преобразование даты рождения в количество полных лет.
+   * 
+   * @param DateTime $dateOfBirth
+   * @return int
+   */
+  static public function convertDateOfBirthInYears(DateTime $dateOfBirth): int
+  {
+    $years = (int)floor((time() - $dateOfBirth->getTimestamp()) / (60 * 60 * 24 * 365));
+    return $years;
+  }
 }
-
-// var_dump(new DateTime('now', new DateTimeZone('Europe/Minsk')));
-// $user = new User(2, 'ggdg', 'gdfgdg', new DateTime('2013-11-02'), 0, 'fgdsgsf');
-$user1 = new User(3);
-// $user->delete()
-// var_dump(User::formatDateOfBirthInYears(new DateTime($user->dateOfBirth)));
-
 
 ?>
